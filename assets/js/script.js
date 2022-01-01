7eaefe276ea898dfef92f7addbb645836ca33ef9
thumbolinat <93739066+thumbolinat@users.noreply.github.com>
2022-01-01 17:46:18 -0500

    updated html and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,40 @@
-var timerEl=document.getElementsByClassName("timer");
+// var timerEl = document.getElementsByClassName("timer");
 
 
-function countdown() {
-  var timeLeft = 60;
-  
+// function countdown() {
+//   var timeLeft = 60;
 
-  var timeInterval = setInterval(function(){
-    if (timeLeft>1){
-      timerEl.text.content = timeLeft;
-      timeLeft--;
-      
-    }else if (timeLeft ===1){
-      timerEl.text.content = timeLeft;
-     timeLeft--;
-    }else {
-      timerEl.text.content="Out of Time";
-      
-      clearInterval(timeInterval);
-    }
-  
-  },1000);
-  }
 
- document.getElementsByName("start").addEventListener("click", countdown());
+//   var timeInterval = setInterval(function () {
+//     if (timeLeft > 1) {
+//       timerEl.text.content = timeLeft;
+//       timeLeft--;
+
+//     } else if (timeLeft === 1) {
+//       timerEl.text.content = timeLeft;
+//       timeLeft--;
+//     } else {
+//       timerEl.text.content = "Out of Time";
+
+//       clearInterval(timeInterval);
+//     }
+
+//   }, 1000);
+// }
+
+// document.getElementsByName("start").addEventListener("click", countdown());
+
+var startButton = document.getElementById("start-button");
+startButton.addEventListener("click", startGame);
+
+
+function startGame(){
+  console.log("started");
+
+}
+function setNextQuestion(){
+
+}
+function selectAnswer(){
+
+}
